2dca78eaa513fc1952a6ac2b73374c078248a763
D.C.Kaushalya <86554652+DC-Kaushalya@users.noreply.github.com>
2023-08-12 23:26:18 +0530

    Handle bus with backend

--- models/bus.js
+++ models/bus.js
@@ -1,55 +1,94 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 
+// const columns: GridColDef[] = [
+// 	{ field: "id", headerName: "ID", width: 90 },
+// 	{
+// 	  field: "img",
+// 	  headerName: "Avatar",
+// 	  width: 100,
+// 	  renderCell: (params) => {
+// 		return <img src={params.row.img || "/noavatar.png"} alt="" />;
+// 	  },
+// 	},
+// 	{
+// 	  field: "firstName",
+// 	  headerName: "First name",
+// 	  type: "string",
+// 	  width: 150,
+// 	  editable: true,
+// 	},
+// 	{
+// 	  field: "lastName",
+// 	  headerName: "Last name",
+// 	  type: "string",
+// 	  width: 150,
+// 	},
+// 	{
+// 	  field: "email",
+// 	  headerName: "Email",
+// 	  type: "string",
+// 	  width: 200,
+// 	},
+// 	{
+// 	  field: "phone",
+// 	  headerName: "Phone",
+// 	  type: "string",
+// 	  width: 150,
+// 	},
+// 	{
+// 	  field: "createdAt",
+// 	  headerName: "Created At",
+// 	  width: 100,
+// 	  type: "string",
+// 	},
+// 	{
+// 	  field: "varified",
+// 	  headerName: "Varified",
+// 	  type: "boolean",
+// 	  width: 100,
+// 	},
+//   ];
+
 const busSchema = new mongoose.Schema({
-	busNumber: {
-		type: String,
-		required: true,
-	},
-	from: {
-		type: String,
-		required: true,
-	},
-	to: {
-		type: String,
-		required: true,
-	},
-	distance: {
-		type: String,
-		required: true,
-	},
-	departureTime: {
-		type: Array,
-		required: true,
-	},
-	arrivalTime: {
-		type: Array,
-		required: true,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	contact: {
-		type: String,
-		required: true,
-	},
+  busNumber: {
+    type: String,
+    required: true,
+  },
+  route: {
+    type: String,
+    required: true,
+  },
+  driver: {
+    type: String,
+    required: true,
+  },
+  contact: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  availability: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 const Bus = mongoose.model("Bus", busSchema);
 
 function validateBus(bus) {
-	const schema = Joi.object({
-		busNumber: Joi.string().required(),
-		from: Joi.string().required(),
-		to: Joi.string().required(),
-		distance: Joi.string().required(),
-		departureTime: Joi.array().required(),
-		arrivalTime: Joi.array().required(),
-		price: Joi.number().required(),
-		contact: Joi.string().required(),
-	});
-	return schema.validate(bus);
+  const schema = Joi.object({
+    busNumber: Joi.string().required(),
+    route: Joi.string().required(),
+    driver: Joi.string().required(),
+    contact: Joi.string().required(),
+    price: Joi.number().required(),
+    availability: Joi.boolean().required(),
+  });
+  return schema.validate(bus);
 }
 
 module.exports.Bus = Bus;
--- routes/bus.js
+++ routes/bus.js
@@ -4,81 +4,79 @@ const router = express.Router();
 const { Bus, validateBus } = require("../models/bus");
 
 router.get("/", async (req, res) => {
-	const busses = await Bus.find();
+  const busses = await Bus.find();
 
-	res.send(busses);
+  res.send(busses);
 });
 
 router.get("/:id", async (req, res) => {
-	const bus = await Bus.findById(req.params.id);
+  const bus = await Bus.findById(req.params.id);
 
-	if (!bus) {
-		return res.send("Bus not found").status(404);
-	}
+  if (!bus) {
+    return res.send("Bus not found").status(404);
+  }
 
-	res.send(bus);
+  res.send(bus);
 });
 
 router.post("/", async (req, res) => {
-	const { error } = validateBus(req.body);
+  const { error } = validateBus(req.body);
 
-	if (error) {
-		return res.send(error.details[0].message).status(400);
-	}
+  if (error) {
+    return res.send(error.details[0].message).status(400);
+  }
 
-	const bus = new Bus({
-		busNumber: req.body.busNumber,
-		from: req.body.from,
-		to: req.body.to,
-		distance: req.body.distance,
-		departureTime: req.body.departureTime,
-		arrivalTime: req.body.arrivalTime,
-		price: req.body.price,
-		contact: req.body.contact,
-	});
+  const bus = new Bus({
+    busNumber: req.body.busNumber,
+    route: req.body.route,
+    driver: req.body.driver,
+    contact: req.body.contact,
+    price: req.body.price,
+    availability: req.body.availability,
+  });
 
-	await bus.save();
+  await bus.save();
 
-	res.send(bus);
+  res.send(bus);
 });
 
 router.put("/:id", async (req, res) => {
-	const { error } = validateBus(req.body);
+  const { error } = validateBus(req.body);
 
-	if (error) {
-		return res.send(error.details[0].message).status(400);
-	}
+  if (error) {
+    return res.send(error.details[0].message).status(400);
+  }
 
-	const bus = await Bus.findByIdAndUpdate(
-		req.params.id,
-		{
-			busNumber: req.body.busNumber,
-			from: req.body.from,
-			to: req.body.to,
-			distance: req.body.distance,
-			departureTime: req.body.departureTime,
-			arrivalTime: req.body.arrivalTime,
-			price: req.body.price,
-			contact: req.body.contact,
-		},
-		{ new: true }
-	);
+  const bus = await Bus.findByIdAndUpdate(
+    req.params.id,
+    {
+      busNumber: req.body.busNumber,
+      from: req.body.from,
+      to: req.body.to,
+      distance: req.body.distance,
+      departureTime: req.body.departureTime,
+      arrivalTime: req.body.arrivalTime,
+      price: req.body.price,
+      contact: req.body.contact,
+    },
+    { new: true }
+  );
 
-	if (!bus) {
-		return res.send("Bus not found").status(404);
-	}
+  if (!bus) {
+    return res.send("Bus not found").status(404);
+  }
 
-	res.send(bus);
+  res.send(bus);
 });
 
 router.delete("/:id", async (req, res) => {
-	const bus = await Bus.findByIdAndDelete(req.params.id);
+  const bus = await Bus.findByIdAndDelete(req.params.id);
 
-	if (!bus) {
-		return res.send("Bus not found").status(404);
-	}
+  if (!bus) {
+    return res.send("Bus not found").status(404);
+  }
 
-	res.send(bus);
+  res.send(bus);
 });
 
 module.exports = router;
